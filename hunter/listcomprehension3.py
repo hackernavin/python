c6095a7943eaac4e683fac05426428071afdb63e
hackernavin <navindhauth@gmail.com>
2017-10-21 11:52:48 +0530

    Add files via upload

---
+++ hunter/listcomprehension3.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      14cse013
+#
+# Created:     21/10/2017
+# Copyright:   (c) 14cse013 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+list = ["navin","babu","sri","deepak","diliban"]
+convert = [x.upper() for x in list]
+print convert
